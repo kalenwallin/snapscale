b27b5fa7e24de115411b5d19043a50823c2bc1f0
Kalen Wallin <31481852+kalenwallin@users.noreply.github.com>
2023-09-05 08:27:26 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="auto_corn_ticket",
+    version="0.1.0",
+    packages=find_packages(),
+    # add any other necessary package metadata
+)
